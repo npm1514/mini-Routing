87493e9bc3593686cd5ad8b086917b3220c94c21
Nick Marucci <npm1514@gmail.com>
2015-12-22 12:34:21 -0700

    Completion of miniproject routing

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('miniRouting', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/home/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'js/settings/settingsTmpl.html',
+            controller: 'settingsCtrl'
+        })
+        .state('products', {
+            url: '/products/:id',
+            templateUrl: 'js/products/productTmpl.html',
+            controller: 'productsCtrl'
+        });
+
+    $urlRouterProvider
+        .otherwise('/');
+});
---
+++ js/products/productsCtrl.js
@@ -0,0 +1,10 @@
+var app = angular.module("miniRouting");
+
+app.controller("productsCtrl", function ($scope, $stateParams, productService) {
+  if ($stateParams.id === "shoes") {
+    $scope.productData = productService.shoeData;
+  }
+  else if ($stateParams.id === "socks") {
+    $scope.productData = productService.sockData;
+  }
+});
